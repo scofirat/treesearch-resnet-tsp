6f57e4508133a85407d74c86b38cba0afd5de578
scofirat <sco.firat@gmail.com>
2018-12-29 20:10:54 +0000

    Add model folder

--- __init__.py
+++ __init__.py
@@ -4,11 +4,11 @@ import nnet
 def main():
     nn=nnet.Estimator("./model")
     searcher=treesearcher(nn)
-    #searcher.search(concordeSteps=10)
+    searcher.search(concordeSteps=10)
     #searcher.search(nnSteps=25)
     #searcher.search(nearest_neighborSteps=5)
     #searcher.search(nnSteps=30)
-    searcher.eval("eval_25_10.csv",plot=True,look_up=25)
+    #searcher.eval("eval_25_10.csv",plot=True,look_up=25)
     #searcher.eval("eval_50_10.csv",plot=False,look_up=50)
     #searcher.eval("eval_100_10.csv",plot=False,look_up=100)
     #searcher.eval("eval_10_10.csv",plot=False,look_up=10)
